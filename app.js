37eac7309b4d77f52391f12f5de83b8813907a2f
eync <eync@users.noreply.github.com>
2020-02-26 15:00:38 +0200

    update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let checkboxy = document.querySelectorAll('.checkboxing');
+let checkboxy2 = document.querySelectorAll('.checkerbox');
 let toggleboxy = document.querySelector('input .checkerbox');
 //!!!! Hyödynnetään myöhemmin
 //let texti = value1;
@@ -6,26 +7,29 @@ let toggleboxy = document.querySelector('input .checkerbox');
 
 //tätä käytetään checklistaukseen
 //pitää korjata tähtäys checkerboxiin koskevaks
-//laita toimimaan addEventListener arrayn ulkopuolella 
+//https://stackoverflow.com/a/32086074
+
+//tarkistetaan onko checkattu ja lisätään tarpeellinen classi tilalle
 function checkifchecked() {
   var checkBox = document.getElementById("myCheck");
-  if (checkBox.checked == true){
   Array.from(checkboxy).forEach((toggleboxy) => { 
     toggleboxy.addEventListener('click', () => {
-      
+      // if (checkBox.checked == true){
       let theclass = ("marked");
       let ifitcontains = toggleboxy.classList.contains(theclass);
-      if (ifitcontains == false) {
+      if (ifitcontains == false && checkBox.checked == true) {
       toggleboxy.classList.toggle(theclass, true); 
-      console.log("Jos ei ole testiä niin lisätään se")
-      } else if (ifitcontains == true) {
+      console.log(ifitcontains);
+      return;
+      } else if (ifitcontains == true && checkBox.checked == false) {
       toggleboxy.classList.toggle(theclass, false);
-      console.log("Jos totta ottaa test classin pois")
-        } return ;
-      }) ; return;
+      console.log(ifitcontains);
+      return;
+        } return;
+  // } 
+  // else if (checkBox.checked == false) { }
+});
   }); 
-} else { console.log("mitä tapahtuu"); return;
 
- };
 }; 
 //vaihda pois ja päälle tietty clässi yksinkertaisesti selitetty
