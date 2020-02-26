0b268e3342f5dbc0cbcf862fc52662a57313b14e
eync <eync@users.noreply.github.com>
2020-02-26 14:17:10 +0200

    update

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 let checkboxy = document.querySelectorAll('.checkboxing');
+let toggleboxy = document.querySelector('input .checkerbox');
 //!!!! Hyödynnetään myöhemmin
 //let texti = value1;
 
 
 //tätä käytetään checklistaukseen
+//pitää korjata tähtäys checkerboxiin koskevaks
+function checkifchecked() {
+  var checkBox = document.getElementById("myCheck");
+  if (checkBox.checked == true){
   Array.from(checkboxy).forEach((toggleboxy) => { 
     toggleboxy.addEventListener('click', (e) => {
-      e.preventDefault();
-      if (toggleboxy.classList.contains("test") == false) {
-      toggleboxy.classList.toggle("test", true); 
+      
+      let theclass = ("marked");
+      let ifitcontains = toggleboxy.classList.contains(theclass);
+      if (ifitcontains == false) {
+      toggleboxy.classList.toggle(theclass, true); 
       console.log("Jos ei ole testiä niin lisätään se")
-      } else if (toggleboxy.classList.contains("test") == true) {
-      toggleboxy.classList.toggle("test", false);
+      } else if (ifitcontains == true) {
+      toggleboxy.classList.toggle(theclass, false);
       console.log("Jos totta ottaa test classin pois")
         } return ;
       });
-  });  
+  }); 
+} else { console.log("mitä tapahtuu"); return;
+
+ };
+}; 
 //vaihda pois ja päälle tietty clässi yksinkertaisesti selitetty
